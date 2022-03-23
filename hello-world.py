1cc4b850786883b9a0ea03f47af09d16651a2290
jboogie <rh@kisasi.com>
2022-03-23 09:11:12 -0400

    update these changes 3/23

--- hello-world.py
+++ hello-world.py
@@ -3,3 +3,5 @@ print("Python has three numeric types: int, float, and complex")
 myValue=False
 
 print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+print("my first update from VS IDE") 
